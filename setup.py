41ce8b4b9eceb4334c477668b9855127b4432853
Udhay-Adithya <164346077+Udhay-Adithya@users.noreply.github.com>
2024-03-21 20:05:38 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vitap_hostel_wifi',
+    version='1.0.0',
+    packages=find_packages(),
+    author='Udhay Adithya',
+    author_email='udhayxd@gmail.com',
+    description='Package for handling hostel wifi login and logout',
+    url='https://github.com/Udhay-Adithya/vitap_hostel_wifi',
+)
